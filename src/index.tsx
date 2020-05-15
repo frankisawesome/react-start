afffe5135a296b99856816d13cace2aeb44efa7e
Frank <b439988l@gmail.com>
2020-05-15 14:21:59 +1000

    init

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+
+const App: FC = () => <h1>Hello</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
